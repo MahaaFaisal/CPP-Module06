40f0ff5197d17e6b226bd930e3fd86af5104ad33
mahaafaisal <mahafaisalmubarak@gmail.com>
2024-10-06 09:09:03 +0400

    checked for double limits

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -5,31 +5,31 @@ ScalarConverter::ScalarConverter(ScalarConverter &other) {(void)other;}
 ScalarConverter &ScalarConverter::operator=(ScalarConverter &rhs) {(void) rhs; return (*this);}
 ScalarConverter::~ScalarConverter(){}
 
-void	ScalarConverter::charToChar(char c)
+void	charToChar(char c)
 {
 	Utils::printMsg("char: ", "magenta");
 	std::cout << '\'' << c << '\'' << std::endl;
 }
 
-void	ScalarConverter::charToInt(char c)
+void	charToInt(char c)
 {
 	Utils::printMsg("int: ", "magenta");
 	std::cout << static_cast<int>(c) << std::endl;
 }
 
-void	ScalarConverter::charToFloat(char c)
+void	charToFloat(char c)
 {
 	Utils::printMsg("float: ", "magenta");
 	std::cout << static_cast<float>(c) << ".0f\n";
 }
 
-void	ScalarConverter::charToDouble( char c)
+void	charToDouble( char c)
 {
 	Utils::printMsg("double: ", "magenta");
 		std::cout << static_cast<double>(c) << ".0\n";	
 }
 
-void	ScalarConverter::numToChar(double scalar)
+void	numToChar(long double scalar)
 {
 	Utils::printMsg("char: ", "magenta");
 	if (scalar < CHAR_MIN || scalar > CHAR_MAX || scalar != static_cast<int>(scalar))
@@ -41,7 +41,7 @@ void	ScalarConverter::numToChar(double scalar)
 
 }
 
-void	ScalarConverter::numToInt(double scalar)
+void	numToInt(long double scalar)
 {
 	Utils::printMsg("int: ", "magenta");
 	if (scalar < INT_MIN || scalar > INT_MAX || isnan(scalar))
@@ -50,7 +50,7 @@ void	ScalarConverter::numToInt(double scalar)
 		std::cout << static_cast<int>(scalar) << std::endl;
 }
 
-void	ScalarConverter::numToFloat(double scalar)
+void	numToFloat(long double scalar)
 {
 	Utils::printMsg("float: ", "magenta");
 	if ((scalar < -std::numeric_limits<float>::max() 
@@ -58,33 +58,38 @@ void	ScalarConverter::numToFloat(double scalar)
 		&& !isinf(scalar))
 		std::cout << "impossible" << std::endl;
 	else
-	std::cout << static_cast<float>(scalar) << ((scalar == static_cast<int>(scalar)) ? ".0f\n" : "f\n");
+		std::cout << static_cast<float>(scalar) << ((scalar == static_cast<int>(scalar)) ? ".0f\n" : "f\n");
 }
 
-void	ScalarConverter::numToDouble(double scalar)
+void	numToDouble(long double scalar)
 {
 	Utils::printMsg("double: ", "magenta");
-	std::cout << scalar << ((scalar == static_cast<int>(scalar)) ? ".0\n" : "\n");	
+	if ((scalar < -std::numeric_limits<double>::max() 
+		|| scalar > std::numeric_limits<double>::max())
+		&& !isinf(scalar))
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << scalar << ((scalar == static_cast<int>(scalar)) ? ".0\n" : "\n");	
 }
 
 void ScalarConverter::convert(std::string str)
 {
 	char *rest;
-	double scalar = strtod(str.c_str(), &rest);
+	long double scalar = strtold(str.c_str(), &rest);
 
 	if (strlen(str.c_str()) == 1 && !isdigit(str[0]))
 	{
-		ScalarConverter::charToChar(str[0]);
-		ScalarConverter::charToInt(str[0]);
-		ScalarConverter::charToDouble(str[0]);
-		ScalarConverter::charToFloat(str[0]);
+		charToChar(str[0]);
+		charToInt(str[0]);
+		charToDouble(str[0]);
+		charToFloat(str[0]);
 	}
 	else if (!*rest || strIsEmpty(rest) || (*rest == 'f' && strIsEmpty(rest + 1)))
 	{
-		ScalarConverter::numToChar(scalar);
-		ScalarConverter::numToInt(scalar);
-		ScalarConverter::numToDouble(scalar);
-		ScalarConverter::numToFloat(scalar);
+		numToChar(scalar);
+		numToInt(scalar);
+		numToDouble(scalar);
+		numToFloat(scalar);
 	}
 	else
 		Utils::printErr("Invalid literal");
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -14,17 +14,6 @@
 
 class ScalarConverter
 {
-	private:
-		static void	charToChar(char c);
-		static void	charToInt(char c);
-		static void	charToFloat(char c);
-		static void	charToDouble(char c);
-
-		static void	numToChar(double scalar);
-		static void	numToInt(double scalar);
-		static void	numToFloat(double scalar);
-		static void	numToDouble(double scalar);
-
 	protected:
 		~ScalarConverter(); // protected to limit instances
 
